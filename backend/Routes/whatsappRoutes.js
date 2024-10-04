f9abef8307ddbdeed161d0b0fef38a868f9d8845
Lucas <lucasvonelm@gmail.com>
2024-10-03 22:19:43 -0300

    Added function to send custom message

--- backend/Routes/whatsappRoutes.js
+++ backend/Routes/whatsappRoutes.js
@@ -5,7 +5,20 @@ import axios from "axios";
 
 const router = express.Router();
 
-router.post("/send-message", async (req, res) => {
+/*
+messaging_product: "whatsapp",
+        to: `${process.env.TEST_PHONE_NUMBER}`,
+        type: "template",
+        template: {
+          name: "hello_world",
+          language: {
+            code: "en_US",
+          },
+        },
+      },
+      */
+
+const sendWhatsAppMessage = async (message) => {
   try {
     const response = await axios({
       url: `https://graph.facebook.com/v20.0/${process.env.WHATSAPP_PHONE_ID}/messages`,
@@ -16,21 +29,43 @@ router.post("/send-message", async (req, res) => {
       },
       data: {
         messaging_product: "whatsapp",
-        to: `${process.env.TEST_PHONE_NUMBER}`,
-        type: "template",
-        template: {
-          name: "hello_world",
-          language: {
-            code: "en_US",
-          },
+        recipient_type: "individual",
+        to: "541136178420",
+        type: "text",
+        text: {
+          preview_url: false,
+          body: message.toString(),
         },
       },
     });
+    return response;
+  } catch (error) {
+    console.log("ERROR HAPPENED");
+    console.log("EEROR TIME: ", new Date());
+    console.log(error);
+    return error;
+  }
+};
+
+router.post("/send-message", async (req, res) => {
+  try {
+    const response = await sendWhatsAppMessage(req.body.message);
 
     return res.status(200).send(response.data);
   } catch (error) {
+    console.log("ERROR HAPPENED");
+    console.log("EEROR TIME: ", new Date());
     console.log(error);
     return res.status(500).send(error);
+  }
+});
+
+router.post("/webhook", async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "Error on server side" });
   }
 });
 
