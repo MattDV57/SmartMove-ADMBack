695567dcc4a5ba3aecbd5dcf3bbbb7e11afd6cc2
Lucas <lucasvonelm@gmail.com>
2024-10-24 23:48:07 -0300

    temporarily added default message to return on all responses

--- utils/templateHandler.js
+++ utils/templateHandler.js
@@ -1,33 +1,36 @@
 const messageTemplateCodeWords = {
   //Funcionalidad
   "Volver a menú": "primer_mensaje_bienvenida",
-  "Terminar el chat": "",
+  "Terminar el chat": "servidores_estado_caido",
 
   //Botones de reclamo
-  "Generar un reclamo": "",
-  "Problema con alquiler": "",
-  "Problema con dueño": "",
-  "Maltrato personal": "",
-  "Intento de estafa": "",
-  "Oferta engañosa": "",
-  "Problema con pagos": "",
-  "Pago no recibido": "",
-  "No puedo enviar pago": "",
-  "Realizar queja": "",
-  "Mal servicio": "",
-  "Mal soporte": "",
-  "Mala experiencia": "",
+  "Generar un reclamo": "servidores_estado_caido",
+  "Problema con alquiler": "servidores_estado_caido",
+  "Problema con dueño": "servidores_estado_caido",
+  "Maltrato personal": "servidores_estado_caido",
+  "Intento de estafa": "servidores_estado_caido",
+  "Oferta engañosa": "servidores_estado_caido",
+  "Problema con pagos": "servidores_estado_caido",
+  "Pago no recibido": "servidores_estado_caido",
+  "No puedo enviar pago": "servidores_estado_caido",
+  "Realizar queja": "servidores_estado_caido",
+  "Mal servicio": "servidores_estado_caido",
+  "Mal soporte": "servidores_estado_caido",
+  "Mala experiencia": "servidores_estado_caido",
 
   //Botones de soporte técnico
   "Soporte técnico": "servidores_estado_caido",
-  "No puedo registrarme": "",
-  "No puedo ingresar": "",
+  "No puedo registrarme": "servidores_estado_caido",
+  "No puedo ingresar": "servidores_estado_caido",
   "Sitio web no funciona": "servidores_estado_caido",
 };
 
 const getTemplateByCode = async (code) => {
   const response = messageTemplateCodeWords[code];
   if (response == undefined) {
+    return "primer_mensaje_bienvenida";
+  }
+  if (response.length == 0) {
     return "primer_mensaje_bienvenida";
   }
   return response;
