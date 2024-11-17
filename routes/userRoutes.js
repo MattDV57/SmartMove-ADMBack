757449a461c0dc9679c48fbe5a1b10a3baa8c774
Ezequiel <ezelusinian@gmail.com>
2024-11-17 18:50:02 -0300

    cambio en el put de usuarios

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -75,8 +75,8 @@ router.put('/:userId', authenticateToken, authorizeRole(ACCESS_CONTROL.PUT_USER)
       { new: true }
     )
 
-    if (savedUser.accessRole = "Admin") {
-      await emitAdminEvent(savedUser, OUTPUT_EVENTS.ADMIN_UPDATED);
+    if (updatedUser.accessRole = "Admin") {
+      await emitAdminEvent(updatedUser, OUTPUT_EVENTS.ADMIN_UPDATED);
     }
 
     return res.status(200).send(updatedUser._id)
