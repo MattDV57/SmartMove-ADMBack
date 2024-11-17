a70a2c08ab91b3652aa7726942100c5492ebc658
Lucas <lucasvonelm@gmail.com>
2024-11-17 17:50:59 -0300

    Moved socketio to index file

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-import 'dotenv/config'
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import { Server } from 'socket.io'
-import { createServer } from 'http'
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-import { pollQueue } from './events/sqsConsumer.js'
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { pollQueue } from "./events/sqsConsumer.js";
+
+import { Chat } from "./models/chatModel.js";
+import { isValidObjectId } from "mongoose";
 
 import cookieParser from "cookie-parser";
 // import chatRoutes from "./Routes/chatRoutes.js";
-import claimRoutes from './routes/claimRoutes.js'
-import loginRoutes from './routes/loginRoutes.js'
-import whatsAppRoutes from './routes/whatsappRoutes.js'
-import authRoutes from './routes/authRoutes.js'
-import logRoutes from './routes/logRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import socketHandler from './sockets/socketHandler.js'
+import claimRoutes from "./routes/claimRoutes.js";
+import loginRoutes from "./routes/loginRoutes.js";
+import whatsAppRoutes from "./routes/whatsappRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import logRoutes from "./routes/logRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -51,7 +53,76 @@ app.use("/auth", authRoutes);
 app.use("/log", logRoutes);
 app.use("/users", userRoutes);
 
-socketHandler(io)
+io.on("connection", (socket) => {
+  socket.on("createChat", async () => {
+    const newChatHistory = await Chat.create({});
+    const chatId = newChatHistory._id.toString();
+    socket.emit("message", chatId);
+
+    socket.join(chatId);
+  });
+
+  socket.on("joinChat", async (chat) => {
+    try {
+      if (!isValidObjectId(chat)) {
+        //ChatId is not a Mongo object Id, it doesnt exist
+        socket.emit("message", "Not a valid room id for chatID: " + chat);
+        return;
+      }
+
+      const isInChat = socket.rooms.has(chat);
+
+      const chatFound = await Chat.findById(chat);
+
+      socket.join(chat);
+
+      if (chatFound && chatFound.messages && chatFound.messages.length > 0) {
+        chatFound.messages.map((message) => {
+          socket.emit("message", message);
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  socket.on("chatMessage", async (messageObject) => {
+    try {
+      const { body, from, sender, chatId } = messageObject;
+      if (!isValidObjectId(chatId)) {
+        return;
+      }
+      const newMessage = {
+        from: from,
+        body: body,
+        sender: sender,
+      };
+
+      const chat = await Chat.findById(chatId);
+      if (!chat || !chat.active) {
+        return;
+      }
+
+      const updatedChat = await Chat.findOneAndUpdate(
+        { _id: chatId },
+        { $push: { messages: newMessage } },
+        { new: true, useFindAndModify: false }
+      );
+
+      if (updatedChat) {
+        socket.join(chatId);
+        io.to(chatId).emit("message", newMessage);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  socket.on("disconnect", () => {
+    console.log(` disconnected`);
+  });
+});
+
+app.set("socketio", io);
 
 // pollQueue();
 
