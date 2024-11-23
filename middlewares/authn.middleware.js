d1014289a34d20e5eea63e66aa817d435a8d5b82
Nacho-93 <ignacioindurainusa@hotmail.com>
2024-11-23 20:41:04 -0300

    console.log token for cookies

--- middlewares/authn.middleware.js
+++ middlewares/authn.middleware.js
@@ -5,11 +5,19 @@ export const authenticateToken = (req, res, next) => {
 
   const token = req.cookies.token
 
+  
+  console.log(token)
+  console.log("ACCESS_TOKEN_SECRET:", process.env.ACCESS_TOKEN_SECRET);
+
   if (!token) {
     return res.status(401).send({ message: 'Access denied. No token provided.' })
   }
 
+  console.log(token)
+console.log("ACCESS_TOKEN_SECRET:", process.env.ACCESS_TOKEN_SECRET);
+
   jsonwebtoken.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log("JWT verification error:", err, "Token:", token, user);
     if (err) return res.sendStatus(403)
     user.accessRole = user.accessRole || user.rol_admin_int.toLowerCase();
     req.user = user;
