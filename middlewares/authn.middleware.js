fa2f72c1378a5c5e6abeafe7d2725b793460bed3
Nacho-93 <ignacioindurainusa@hotmail.com>
2024-11-25 01:12:04 -0300

    fixed cookie for admin

--- middlewares/authn.middleware.js
+++ middlewares/authn.middleware.js
@@ -11,7 +11,7 @@ export const authenticateToken = (req, res, next) => {
 
   jsonwebtoken.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403)
-    user.accessRole = user.accessRole || user.rol_admin_int.toLowerCase();
+    user.accessRole = user.accessRole || user.rol_admin_int ? user.rol_admin_int.toLowerCase() : user.is_admin ? "admin" : undefined;
     user.userId = user.userId || user.cuit;
     req.user = user;
     next()
